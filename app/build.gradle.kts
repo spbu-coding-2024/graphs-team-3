fe2f16aa32dbced5892b14e93e86eef5b1ce5d7e
Stepan Gorlov <Stepagor105@gmail.com>
2025-04-20 14:17:46 +0300

    build: add Gradle setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
+    id("org.jetbrains.compose") version "1.7.1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    google()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://raw.github.com/gephi/gephi/mvn-thirdparty-repo/")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    testImplementation(kotlin("test"))
+    implementation("org.xerial:sqlite-jdbc:3.49.1.0")
+    implementation("org.neo4j.driver", "neo4j-java-driver", "5.28.4")
+    implementation("org.gephi","gephi-toolkit" ,"0.10.1" , classifier = "all")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+
+}
