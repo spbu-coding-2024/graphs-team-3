5d4d66bdb21522126b2f97a0bc3c2691b901d73d
Stepan Gorlov <Stepagor105@gmail.com>
2025-05-05 12:29:54 +0300

    feat: add edges colors

--- app/src/main/kotlin/viewmodel/graph/EdgeViewModel.kt
+++ app/src/main/kotlin/viewmodel/graph/EdgeViewModel.kt
@@ -1,17 +1,28 @@
 package viewmodel.graph
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 import model.graph.Edge
 
 class EdgeViewModel (
     val first: VertexViewModel,
     val second: VertexViewModel,
+    color: Color,
     private val edge: Edge,
     private val _weightVisible: State<Boolean>,
+    private val isDirected: Boolean
 ) {
     val weight
         get() = edge.weight.toString()
 
     val weightVisible
         get() = _weightVisible.value
+
+    private var _color = mutableStateOf(color)
+    var color: Color
+        get() = _color.value
+        set(value) {
+            _color.value = value
+        }
 }
