e9b374dc4e565370fbfe2f811eb78a7fa671aff4
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-05-09 01:19:50 +0300

    feat: add funcs for algos in vm main screen

--- app/src/main/kotlin/viewmodel/screens/MainScreenViewModel.kt
+++ app/src/main/kotlin/viewmodel/screens/MainScreenViewModel.kt
@@ -5,10 +5,17 @@ import androidx.compose.ui.graphics.Color
 import model.graph.Graph
 import viewmodel.graph.GraphViweModel
 import viewmodel.representation.RepresentationStrategy
+import model.algo.*
+import model.algo.findBridges.findBridges
+import model.algo.findSCC
+import model.io.sqlite.SqliteRepository
+import kotlin.random.Random
 
 class MainScreenViewModel (
-    graph: Graph,
-    private val representationStrategy: RepresentationStrategy
+    private val graph: Graph,
+    private val representationStrategy: RepresentationStrategy,
+    private var sqliteRepo: SqliteRepository? = null,
+    private var savedId: Int? = null,
 ) {
     private var _showVerticesLabels = mutableStateOf(false)
     var showVerticesLabels: Boolean
@@ -41,5 +48,51 @@ class MainScreenViewModel (
     fun resetGraphView() {
         representationStrategy.place(800.0, 600.0, graphViewModel)
         graphViewModel.vertices.forEach { v -> v.color = Color.Gray }
+    }
+
+    fun showBridges() {
+        val edges = findBridges(graph)
+        graphViewModel.edges.forEach { e ->
+            if (e.origin in edges) e.color = Color.Red
+        }
+    }
+
+    fun showMst() {
+        val mst = findMST(graph)
+        graphViewModel.edges.forEach { e ->
+            e.color = if (e.origin in mst) Color.Green else Color.LightGray
+        }
+    }
+
+    fun showCommunities() {
+        val comm = labelPropagation(graph)
+        val palette = generateSequence { Color(Random.nextInt()) }.distinct().iterator()
+        comm.values.forEach { verts ->
+            val c = palette.next()
+            verts.forEach { v ->
+                graphViewModel.vertices.first { it.origin == v }.color = c
+            }
+        }
+    }
+
+    fun showScc() {
+        val scc = findSCC(graph)
+        val palette = generateSequence { Color(Random.nextInt()) }.distinct().iterator()
+        scc.forEach { comp ->
+            val c = palette.next()
+            comp.forEach { v ->
+                graphViewModel.vertices.first { it.origin == v }.color = c
+            }
+        }
+    }
+
+    fun saveToDb(name: String? = null) {
+        sqliteRepo ?: return
+        savedId = if (savedId == null)
+            sqliteRepo!!.save(graph, name)
+        else {
+            sqliteRepo!!.update(savedId!!, graph, name)
+            savedId
+        }
     }
 }
