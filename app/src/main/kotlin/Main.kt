e00b2f59573369b654ee93337bb6a0419b471110
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-05-10 01:24:56 +0300

    feat: support sqliteRepo and graphId in MainScreenNav

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -11,6 +11,7 @@ import view.screens.helloScreen
 import viewmodel.representation.RepresentationStrategy
 import viewmodel.screens.MainScreenViewModel
 import java.awt.Dimension
+import model.io.sqlite.SqliteRepository
 
 
 object HelloScreen:  Screen {
@@ -18,9 +19,21 @@ object HelloScreen:  Screen {
     override fun Content() = helloScreen()
 }
 
-class MainScreenNav(val graph: Graph, private val representationStrategy: RepresentationStrategy): Screen {
+class MainScreenNav(
+    val graph: Graph,
+    private val representationStrategy: RepresentationStrategy,
+    val sqliteRepo: SqliteRepository? = null,
+    val graphId: Int? = null
+): Screen {
     @Composable
-    override fun Content() = MainScreen(MainScreenViewModel(graph, representationStrategy))
+    override fun Content() = MainScreen(
+        MainScreenViewModel(
+            graph,
+            representationStrategy,
+            sqliteRepo,
+            graphId,
+            )
+    )
 }
 
 @Composable
