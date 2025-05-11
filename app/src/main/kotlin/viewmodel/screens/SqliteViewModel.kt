94e64cd3a195938caf019d8c0526c71b387c0f0a
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-05-11 17:48:10 +0300

    feat: add sqlite viewmodel

---
+++ app/src/main/kotlin/viewmodel/screens/SqliteViewModel.kt
@@ -0,0 +1,26 @@
+package viewmodel.screens
+
+import androidx.compose.runtime.*
+import model.graph.Graph
+import model.io.sqlite.SqliteRepository
+
+class SqliteViewModel(private val repo: SqliteRepository) {
+
+    var filter by mutableStateOf("")
+        private set
+    val graphs = mutableStateListOf<Pair<Int, String>>()
+
+    init { reload() }
+
+    fun onFilterChange(searchQuery: String) {
+        filter = searchQuery
+        reload()
+    }
+
+    private fun reload() {
+        graphs.clear()
+        graphs += repo.listGraphs(filter)
+    }
+
+    fun openGraph(id: Int): Graph = repo.read(id)
+}
