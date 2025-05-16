43ad46a3cee23c9927610df43af11ad365238ab0
Stepan Gorlov <Stepagor105@gmail.com>
2025-05-16 22:11:35 +0300

    feat: add random graph generate function

---
+++ app/src/main/kotlin/model/utils/RandomGraph.kt
@@ -0,0 +1,31 @@
+package model.utils
+
+import model.graph.Graph
+import model.io.neo4j.Neo4jRepository
+
+fun randomGraph(
+    isDirected: Boolean,
+    isWeighted: Boolean,
+    vertexCount: Int,
+    edgeMaxCount: Int,
+    maxWeight: Long = 1,
+): Graph {
+    var resultGraph = Graph(isDirected)
+    var edges = mutableSetOf<Pair<Int, Int>>()
+
+    for (i in 0 until vertexCount) {
+        resultGraph.addVertex(i.toString())
+    }
+
+    for (i in 0 until vertexCount) {
+        for (j in (0 until vertexCount).shuffled().take((1 .. edgeMaxCount).random())) {
+            if (isDirected && edges.contains(j to i)) {
+                continue
+            }
+            resultGraph.addEdge(i, j, (1 .. maxWeight).random())
+            edges.add(i to j)
+        }
+    }
+
+    return  resultGraph
+}
