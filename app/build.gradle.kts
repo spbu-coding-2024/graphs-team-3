6da2bb7265078bc68623bd89781eafc3a66ef3eb
Stepan Gorlov <Stepagor105@gmail.com>
2025-05-29 21:40:06 +0300

    refactor: delete comments from build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:3.49.1.0")
     implementation("org.neo4j.driver", "neo4j-java-driver", "5.28.4")
     implementation("org.gephi","gephi-toolkit" ,"0.10.1" , classifier = "all")
-//    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("cafe.adriel.voyager:voyager-navigator:1.0.1")
     implementation("cafe.adriel.voyager:voyager-screenmodel:1.0.1")
     implementation("org.jetbrains.exposed:exposed-core:0.61.0")
